c75a639fb1f3a46fdad4bb787dca1da9fc8b61a0
Jeff-Maina <devyoungblud@gmail.com>
2024-09-27 14:05:10 +0300

    add bubble menu

--- src/app/_components/editor.tsx
+++ src/app/_components/editor.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEditor, EditorContent } from "@tiptap/react";
+import { useEditor, EditorContent, BubbleMenu } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Document from "@tiptap/extension-document";
 import Paragraph from "@tiptap/extension-paragraph";
@@ -8,10 +8,12 @@ import Text from "@tiptap/extension-text";
 import Bold from "@tiptap/extension-bold";
 import Strike from "@tiptap/extension-strike";
 import Link from "@tiptap/extension-link";
+import suggestion from "@tiptap/suggestion";
 import Code from "@tiptap/extension-code";
 import OrderedList from "@tiptap/extension-ordered-list";
 import ListItem from "@tiptap/extension-list-item";
 import Blockquote from "@tiptap/extension-blockquote";
+import Mention from "@tiptap/extension-mention";
 import Placeholder from "@tiptap/extension-placeholder";
 
 import {
@@ -301,7 +303,10 @@ const EditorComp = () => {
             <div className="flex divide-x items-center">
               <div className="flex items-center pr-1.5">
                 <TooltipWrapper sideOffset={5} align="center" label="Attach">
-                  <button className="size-7 mr-1.5 grid place-items-center rounded-full bg-neutral-200/70 hover:bg-neutral-200 text-neutral-600 hover:text-black">
+                  <button
+                    disabled={true}
+                    className="size-7 mr-1.5 grid place-items-center rounded-full bg-neutral-200/70 hover:bg-neutral-200 text-neutral-600 hover:text-black disabled:cursor-not-allowed"
+                  >
                     <Plus size={16} />
                   </button>
                 </TooltipWrapper>
@@ -339,7 +344,10 @@ const EditorComp = () => {
                   label={"Record video clip"}
                   align="center"
                 >
-                  <button className="px-2  text-neutral-500 hover:text-black transition-all">
+                  <button
+                    disabled={true}
+                    className="px-2  disabled:cursor-not-allowed disabled:text-neutral-300  text-neutral-500 hover:text-black transition-all"
+                  >
                     <Video size={18} />
                   </button>
                 </TooltipWrapper>
@@ -348,7 +356,10 @@ const EditorComp = () => {
                   label={"Record audio clip"}
                   align="center"
                 >
-                  <button className="px-2  text-neutral-500 hover:text-black transition-all">
+                  <button
+                    disabled={true}
+                    className="px-2  disabled:cursor-not-allowed disabled:text-neutral-300  text-neutral-500 hover:text-black transition-all"
+                  >
                     <Mic size={18} />
                   </button>
                 </TooltipWrapper>
@@ -359,7 +370,10 @@ const EditorComp = () => {
                   label={"Run shortcut"}
                   align="center"
                 >
-                  <button className="px-1.5  text-neutral-500 hover:text-black transition-all">
+                  <button
+                    disabled={true}
+                    className="px-2  disabled:cursor-not-allowed disabled:text-neutral-300  text-neutral-500 hover:text-black transition-all"
+                  >
                     <SquareSlash size={18} />
                   </button>
                 </TooltipWrapper>
@@ -377,6 +391,158 @@ const EditorComp = () => {
               </TooltipWrapper>
             </div>
           </div>
+
+          {/* bubble menu */}
+          {editor && (
+            <BubbleMenu
+            
+              className={cn(
+                "-translate-y-2",
+                formatMenuVisible ||
+                  (isLinkModalOpen &&
+                    "hidden pointer-events-none absolute -z-[999]")
+              )}
+              editor={editor}
+              tippyOptions={{ duration: 100 }}
+            >
+              <div className="bubble-menu bg-neutral-900 divide-x py-1.5 divide-neutral-600 flex items-center rounded-md ">
+                <div className="flex gap-1.5 px-1.5">
+                  <TooltipWrapper sideOffset={10} label="Bold" align="center">
+                    <button
+                      onClick={() => editor?.chain().focus().toggleBold().run()}
+                      className={cn(
+                        "bubble_button text-neutral-100",
+                        editor?.isActive("bold") ? "is-bb-active" : ""
+                      )}
+                    >
+                      <BoldIcon size={16} />
+                    </button>
+                  </TooltipWrapper>
+                  <TooltipWrapper sideOffset={10} label="Italic" align="center">
+                    <button
+                      onClick={() =>
+                        editor?.chain().focus().toggleItalic().run()
+                      }
+                      className={cn(
+                        "bubble_button text-neutral-100",
+                        editor?.isActive("italic") ? "is-bb-active" : ""
+                      )}
+                    >
+                      <Italic size={16} />
+                    </button>
+                  </TooltipWrapper>
+                  <TooltipWrapper
+                    sideOffset={10}
+                    label="Strikethrough"
+                    align="center"
+                  >
+                    <button
+                      onClick={() =>
+                        editor?.chain().focus().toggleStrike().run()
+                      }
+                      className={cn(
+                        "bubble_button text-neutral-100",
+                        editor?.isActive("strike") ? "is-bb-active" : ""
+                      )}
+                    >
+                      <Strikethrough size={16} />
+                    </button>
+                  </TooltipWrapper>
+                </div>
+                <div className="flex gap-1.5 px-1.5">
+                  <TooltipWrapper sideOffset={10} label="Bold" align="center">
+                    <button
+                      onClick={openLinkModal}
+                      className={cn(
+                        "bubble_button text-neutral-100",
+                        editor?.isActive("link") ? "is-bb-active" : ""
+                      )}
+                    >
+                      <LinkIcon size={16} />
+                    </button>
+                  </TooltipWrapper>
+                </div>
+                <div className="flex gap-1.5 px-1.5">
+                  <TooltipWrapper
+                    sideOffset={10}
+                    label="Ordered List"
+                    align="center"
+                  >
+                    <button
+                      onClick={() =>
+                        editor?.chain().focus().toggleOrderedList().run()
+                      }
+                      className={cn(
+                        "bubble_button text-neutral-100",
+                        editor?.isActive("orderedList") ? "is-bb-active" : ""
+                      )}
+                    >
+                      <ListOrderedIcon size={16} />
+                    </button>
+                  </TooltipWrapper>
+                  <TooltipWrapper sideOffset={10} label="List" align="center">
+                    <button
+                      onClick={() =>
+                        editor?.chain().focus().toggleBulletList().run()
+                      }
+                      className={cn(
+                        "bubble_button text-neutral-100",
+                        editor?.isActive("bulletList") ? "is-bb-active" : ""
+                      )}
+                    >
+                      <List size={16} />
+                    </button>
+                  </TooltipWrapper>
+                </div>
+                <div className="flex gap-1.5 px-1.5">
+                  <TooltipWrapper
+                    sideOffset={10}
+                    label="Blockquote"
+                    align="center"
+                  >
+                    <button
+                      onClick={() =>
+                        editor?.chain().focus().toggleBlockquote().run()
+                      }
+                      className={cn(
+                        "bubble_button text-neutral-100",
+                        editor?.isActive("blockquote") ? "is-bb-active" : ""
+                      )}
+                    >
+                      <TextQuote size={16} />
+                    </button>
+                  </TooltipWrapper>
+                </div>
+                <div className="flex gap-1.5 px-1.5">
+                  <TooltipWrapper sideOffset={10} label="Code" align="center">
+                    <button
+                      onClick={() => editor?.chain().focus().toggleCode().run()}
+                      className={cn(
+                        "bubble_button text-neutral-100 disabled:text-neutral-500 disabled:cursor-not-allowed",
+                        editor?.isActive("code") ? "is-bb-active" : ""
+                      )}
+                      disabled={editor?.isActive("codeBlock")}
+                    >
+                      <CodeIcon size={16} />
+                    </button>
+                  </TooltipWrapper>{" "}
+                  <TooltipWrapper sideOffset={10} label="Bold" align="center">
+                    <button
+                      onClick={() =>
+                        editor?.chain().focus().toggleCodeBlock().run()
+                      }
+                      className={cn(
+                        "bubble_button text-neutral-100",
+                        editor?.isActive("codeBlock") ? "is-bb-active" : ""
+                      )}
+                    >
+                      <SquareTerminal size={16} />
+                    </button>
+                  </TooltipWrapper>
+                </div>
+              </div>
+            </BubbleMenu>
+          )}
         </div>
       </div>
 
